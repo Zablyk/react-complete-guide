43d8dda73b757b2061ccbb77a7c5a07c1d5db3cf
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-11-01 17:33:32 -0400

    adding a new component to our code

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
+     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1> 
+        <p>This is really working!</p>
       </div>
     );
+    //return React.createElement('div', { className: 'App'}, React.createElement('h1', null, 'Does this one work now?'));
   }
 }
 
