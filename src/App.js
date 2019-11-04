f0b473ecef6f3c73611ad5b50a38e578ce63db47
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-11-04 18:46:27 -0500

    adding choldren props

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1> 
         <p>This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Max" age="29"/>
+        <Person name="Oleg" age="33">My hobbies: Racing</Person>
+        <Person name="Stephanie" age="24"/>
       </div>
     );
     //return React.createElement('div', { className: 'App'}, React.createElement('h1', null, 'Does this one work now?'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const person = () => {
-    return <p>I'm a person and I'm a {Math.floor(Math.random() * 30)} years old!</p>
+const person = (props) => {
+    return(
+        <div>
+            <p>I'm {props.name} and I'm a {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
+    );
 };
 
 export default person;
