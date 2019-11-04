20140ca5e0a555772750b4eb3feb8ee316e36e8d
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-11-04 14:10:20 -0500

    adding dynamic components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = () => {
-    return(
-        <p>I'm a person</p>
-    )
-}
+    return <p>I'm a person and I'm a {Math.floor(Math.random() * 30)} years old!</p>
+};
+
+export default person;
