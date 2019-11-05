62029eb1ca393d15eb40567faf920e1d75dfac4b
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-11-04 20:49:51 -0500

    switched from setState to ReactHooks

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 
-class App extends Component {
-  state = {
-   persons: [
-    { name: 'Oleg', age: 39 },
-    { name: 'Manu', age: 29 },
-    { name: 'Stephanie', age: 24 },
-   ]
-  }
+const app = props => {
+  const [personsState, setPersonsSate] = useState( {
+    persons: [
+     { name: 'Oleg', age: 39 },
+     { name: 'Manu', age: 29 },
+     { name: 'Stephanie', age: 24 },
+    ],
+    otherState: 'some other value'
+   });
+
+   
+ const switchNameHandler = () => {
+  //console.log('Was clicked!');
+  //DON'T DO THIS: personsState.persons[0].name = 'Maximilian';
+  setPersonsSate({
+    persons: [
+      { name: 'Maximilian', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 27 },
+     ]
+  });
+};
   
-  switchNameHandler = () => {
-    console.log('Was clicked!');
-  }
-  
-  render() {
      return (
       <div className="App">
         <h1>Hi, I'm a React App</h1> 
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name= {this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={switchNameHandler}>Switch name</button>
+        <Person name= {personsState.persons[0].name} age={personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobbies: Racing</Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
       </div>
     );
     //return React.createElement('div', { className: 'App'}, React.createElement('h1', null, 'Does this one work now?'));
-  }
-}
+    }
 
-export default App;
+export default app;
+
