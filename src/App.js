b81303226cc66aaaa3f919112b664455413e7e3d
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-11-04 20:00:28 -0500

    setting event listener

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+
 class App extends Component {
+  state = {
+   persons: [
+    { name: 'Oleg', age: 39 },
+    { name: 'Manu', age: 29 },
+    { name: 'Stephanie', age: 24 },
+   ]
+  }
+  
+  switchNameHandler = () => {
+    console.log('Was clicked!');
+  }
+  
   render() {
      return (
       <div className="App">
         <h1>Hi, I'm a React App</h1> 
         <p>This is really working!</p>
-        <Person name="Max" age="29"/>
-        <Person name="Oleg" age="33">My hobbies: Racing</Person>
-        <Person name="Stephanie" age="24"/>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name= {this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     //return React.createElement('div', { className: 'App'}, React.createElement('h1', null, 'Does this one work now?'));
