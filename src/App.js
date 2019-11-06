04f6fbba6829e3318bf7a1a052465ccac9691cdc
Oleg Zablotskyi <zablotsky@gmail.com>
2019-11-05 21:54:43 -0500

    added a bind method to a switchNameHandler

--- src/App.js
+++ src/App.js
@@ -16,12 +16,12 @@ class App extends Component {
   
 
    
- switchNameHandler = () => {
+ switchNameHandler = (newName) => {
   //console.log('Was clicked!');
   //DON'T DO THIS: personsState.persons[0].name = 'Maximilian';
   this.setState({
     persons: [
-      { name: 'Maximilian', age: 28 },
+      { name: newName, age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 27 },
      ]
@@ -32,14 +32,14 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1> 
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch name</button>
+        <button onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch name</button>
         <Person
           name= {this.state.persons[0].name}
           age={this.state.persons[0].age}/>
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler}>My hobbies: Racing</Person>
+          click={this.switchNameHandler.bind(this, 'Maxim')}>My hobbies: Racing</Person>
         <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age}/>
