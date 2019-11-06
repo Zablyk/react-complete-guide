cac6a8fb8d3e9e632924b6ed976b41cd037d71b5
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-11-06 18:21:24 -0500

    added a const style in to js file

--- src/App.js
+++ src/App.js
@@ -39,11 +39,22 @@ nameChangedHandler = (event) => {
 };
  
     render () {
+
+      const style = {
+        backgroundColor: 'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px',
+        cursor: 'pointer'
+      };
+
      return (
       <div className="App">
         <h1>Hi, I'm a React App</h1> 
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch name</button>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler('Maximilian!!')}>Switch name</button>
         <Person
           name= {this.state.persons[0].name}
           age={this.state.persons[0].age}/>
