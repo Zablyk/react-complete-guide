24819d61934d24545d907d45968ebc0414a91672
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-11-06 17:44:40 -0500

    added Person.css with a styling

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import './Person.css';
+
 const person = (props) => {
     return(
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I'm a {props.age} years old!</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.changed} />
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
     );
 };
