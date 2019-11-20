7d7b309d2961b34f49113ba2464e30d33683e9c1
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-11-20 14:15:40 -0500

    added ErrorBoundary.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,6 +4,11 @@ import classes from './Person.css';
 
 const person = (props) => {
 
+  const rnd = Math.random();
+  if (rnd > 0.7) {
+    throw new Error('Somethink went wrong');
+  }
+
     return(
         <div className = {classes.Person}>
           <p onClick={props.click}>
