b03726f96394ee691a96e078aa4e9c6850d08745
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-12-09 20:09:21 -0500

    converted functional base componet into class-base component in Person

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
 
 
-const person = (props) => {
-  console.log('[Person.js] rendering...')
+class Person extends Component {
+  render() {
+    console.log('[Person.js] rendering...')
     return(
         <div className = {classes.Person}>
-          <p onClick={props.click}>
-          I'm {props.name} and I'm a {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
+          <p onClick={this.props.click}>
+          I'm {this.props.name} and I'm a {this.props.age} years old!</p>
+            <p>{this.props.children}</p>
+            <input type="text" onChange={this.props.changed} value={this.props.name} />
         </div>
       );
+  }
 };
 
-export default person;
+export default Person;
