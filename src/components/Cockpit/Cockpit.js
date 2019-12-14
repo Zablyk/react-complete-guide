41d584f95a2008a975d9c09f930bbae3b31173d7
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-12-14 15:54:19 -0500

    cleanup Work Wit useEffect()

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,14 +1,15 @@
 import React, { useEffect} from 'react';
 import classes from './Cockpit.css';
 
-const cockpit = (props) => {
+const cockpit = props => {
     useEffect(() => {
       console.log('[Cockpit.js] useEffect');
       //HTTP request...
-      setTimeout(() => {
+      const timer = setTimeout(() => {
         alert('Saved data to cloud!');
       }, 1000)
       return () => {
+        clearTimeout(timer);
         console.log('[Cockpit.js] cleanup work in useEffect')
       };
       
