c718711cc0c4d302fccf596cfbdc77158f1b825f
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-12-17 18:28:58 -0500

    added empty wraper Aux in Auxiliary.js and took out array from return in Person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
+
+import Aux from '../../../hoc';
 import classes from './Person.css';
 
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...')
-    return [
-       
+    return (
+        <Aux> 
           <p key="i1" onClick={this.props.click}>
-          I'm {this.props.name} and I'm a {this.props.age} years old!</p>,
-            <p key="i2">{this.props.children}</p>,
+          I'm {this.props.name} and I'm a {this.props.age} years old!</p>
+            <p key="i2">{this.props.children}</p>
             <input
             key="i3"
             type="text"
             onChange={this.props.changed}
             value={this.props.name} />
-        
-      ];
+        </Aux>
+        );
   }
 };
 
