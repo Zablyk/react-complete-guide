8022e496b73e4ba2153d46d9e739f57ad1518bb6
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-01-02 14:12:44 -0500

    chenging a WithClass to a hoc functional component

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 import WithClass from '../hoc/WithClass';
+import Aux from '../hoc/Auxiliary';
 
 
 class App extends Component {
--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
-const withClass = props => (
-    <div className={props.classes}>{props.children}</div>
-    );
+const withClass = (WrappedComponent, ClassName) => {
+    return props => (
+        <div className={className}>
+            <WrappedComponent />
+        </div>
+    ); 
+};
+
+
 export default withClass
