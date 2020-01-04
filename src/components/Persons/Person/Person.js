636f8873a2c625bc3819ba1b11a64ae87a1f3452
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-01-04 16:16:35 -0500

    added ref for focus on input field using constructor

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,8 +7,14 @@ import classes from './Person.css';
 
 
 class Person extends Component {
+constructor(props) {
+  super(props);
+  this.inputElementRef = React.createRef();
+}
+
   componentDidMount() {
-    this.inputElement.focus();
+    //this.inputElement.focus();
+    this.inputElementRef.current.focus();
   }
 
   render() {
@@ -20,7 +26,8 @@ class Person extends Component {
             <p key="i2">{this.props.children}</p>
             <input
             key="i3"
-            ref={(inputEl) => {this.inputElement = inputEl}}
+            //ref={(inputEl) => {this.inputElement = inputEl}}
+            ref={this.inputElementRef}
             type="text"
             onChange={this.props.changed}
             value={this.props.name} />
