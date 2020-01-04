c0d37c59d93f0874645efd8634f7789c8be7f23d
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-01-04 11:00:55 -0500

    new changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -20,7 +20,8 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
    };
 
    static getDerivedStateFromProps(props, state) {
@@ -57,7 +58,12 @@ nameChangedHandler = (event, id) => {
   const persons = [...this.state.persons];
   persons[personIndex] = person;
 
-  this.setState({ persons: persons});
+  this.setState ((prevState, props) => {
+    return {
+      persons: persons,
+      changeCounter: this.state.changeCounter + 1
+      };
+    });
 };
 
 deletePersonHandler = (personIndex) => {
